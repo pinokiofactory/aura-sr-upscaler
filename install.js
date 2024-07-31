90684185285662ad12536c1813be335b242ba065
cocktailpeanut <cocktailpeanuts@proton.me>
2024-07-31 08:21:28 -0400

    update

--- install.js
+++ install.js
@@ -28,7 +28,7 @@ module.exports = {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "pip install gradio gradio-imageslider devicetorch",
+          "pip install gradio gradio-imageslider devicetorch spaces",
           "pip install aura-sr@git+https://github.com/cocktailpeanut/aura-sr.git"
         ]
       }
